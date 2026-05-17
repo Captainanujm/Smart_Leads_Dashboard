792b02ca6793bc44d5208d25bde4f8b38f7240f4
Anuj mishra <captainanuj2004@gmail.com>
2026-05-17 10:33:59 +0530

    increased search debounce delay to 500ms

--- client/src/hooks/useDebounce.ts
+++ client/src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export function useDebounce<T>(value: T, delay: number = 300): T {
+export function useDebounce<T>(value: T, delay: number = 500): T {
   const [debounced, setDebounced] = useState(value);
 
   useEffect(() => {
